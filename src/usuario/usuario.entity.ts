72997efe366808c03e81ddc7655a228115b8fd60
Daniel Ribeiro <109773063+DevJuvenilson@users.noreply.github.com>
2025-02-01 20:39:18 -0300

    Update imports from usuario.entity

--- src/usuario/usuario.entity.ts
+++ src/usuario/usuario.entity.ts
@@ -1,4 +1,11 @@
-import { Entity, Column, CreateDateColumn, UpdateDateColumn, DeleteDateColumn, PrimaryGeneratedColumn } from "typeorm";
+import { 
+    Entity, 
+    Column, 
+    CreateDateColumn, 
+    UpdateDateColumn, 
+    DeleteDateColumn, 
+    PrimaryGeneratedColumn 
+} from "typeorm";
 
 @Entity({ name: 'usuarios' })
 export class UsuarioEntity {
